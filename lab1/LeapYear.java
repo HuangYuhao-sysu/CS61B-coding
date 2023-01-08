df1dd8353d2c613a765320c3cc38ff6b235190fd
HuangYuhao <huangyh76@mail2.sysu.edu.cn>
2023-01-08 16:54:18 +0800

    lab1 complete

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,6 +2,22 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
+    /**
+     * Determine if the param year is a leap year.
+     * @param year
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } 
+        
+        if ((year % 4 == 0) && (year % 100 != 0)) {
+            return true;
+        }
+
+        return false;
+    }
+
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
