7903cf0cad98677e9c7268b31b89f9c19cfa14a3
Huang Yuhao <huangyh76@mail2.sysu.edu.cn>
2023-04-21 10:49:45 +0800

    ok

--- disc04/IntListTest.java
+++ disc04/IntListTest.java
@@ -3,4 +3,14 @@ public class IntListTest {
      * Advantage: We can test the different sub method in class separately.
      * Disadvantage: No.
      */
+
+     /** Answer:
+      * Advantages:
+        1. Keeps your code organized - each test corresponds to different building blocks of your program.
+        2. You can debug your code locally and find which part of your program is not working.
+        3. Provides documentation that your program actually words.
+        4. Can reduce the amount of test code you need to write because you can reuse code.
+      * Disadvantages:
+        1. Hard to use for higher-level testing.
+      */
 }
