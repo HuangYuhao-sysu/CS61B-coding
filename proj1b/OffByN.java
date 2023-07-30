0ce2ecc3cd37ce3347efbb973353b3d69648625e
Yuhao Huang <18813946109@163.com>
2023-07-30 21:22:16 +0800

    完成proj1b

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,10 +1,2 @@
-public class OffByN implements CharacterComparator {
-    public int N;
-    public OffByN(int x) {
-       N = x;
-    }
-    @Override
-    public boolean equalChars(char x, char y) {
-        return ((int) x + this.N) == (int) y || ((int) x - this.N) == (int) y;
-    }
+package PACKAGE_NAME;public class OffByN {
 }
