4fe51be5bc5e07d4cad7f009b9023935655d33ec
Yuhao Huang <18813946109@163.com>
2023-07-30 22:38:25 +0800

    ok

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class TestArrayDequeGold {
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+public class TestArrayDequeGold {
+    static StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+    static ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+
+    public static void initialDeque() {
+        for (int j = 0; j < 10; j += 1) {
+            int addFirstItem = StdRandom.uniform(10);
+            int addLastItem = StdRandom.uniform(10);
+            sad.addFirst(addFirstItem);
+            ads.addFirst(addFirstItem);
+            sad.addLast(addLastItem);
+            ads.addLast(addLastItem);
+        }
+    }
+
+    @Test
+    public static void main(String[] args) {
+        initialDeque();
+        for (int i = 0; i < 9; i += 1) {
+            sad.removeFirst();
+            ads.removeFirst();
+            sad.removeLast();
+            ads.removeLast();
+        }
+        sad.printDeque();
+        ads.printDeque();
+    }
+
 }
